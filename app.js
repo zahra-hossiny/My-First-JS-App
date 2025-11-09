c5d35430b7b2a9f7903cb6ede57b7937e6607c30
zahra-hossiny <zhalsadat.7676@gmail.com>
2025-11-09 12:03:28 +0330

    feat: Add initial HTML, CSS, and JavaScript files

---
+++ app.js
@@ -0,0 +1,6 @@
+// اولین تمرین جاوااسکریپت
+console.log("فایل جاوااسکریپت با موفقیت متصل شد.");
+
+// مثال: تغییر متن المان HTML با استفاده از JS (DOM Manipulation)
+const element = document.getElementById('output');
+element.textContent = "جاوااسکریپت فعال است! (نشان داده شده در صفحه)";
